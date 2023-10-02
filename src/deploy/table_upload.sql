dc2d59233eb108100aa8ae1d4580de16d31971bb
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-10-02 06:33:03 +0200

    fix(upload): show for profile pictures

--- src/deploy/table_upload.sql
+++ src/deploy/table_upload.sql
@@ -2,6 +2,7 @@
 -- requires: schema_public
 -- requires: table_account_public
 -- requires: role_account
+-- requires: role_anonymous
 -- requires: role_tusd
 
 BEGIN;
@@ -19,17 +20,22 @@ COMMENT ON COLUMN maevsi.upload.account_id IS 'The uploader''s account id.';
 COMMENT ON COLUMN maevsi.upload.size_byte IS 'The upload''s size in bytes.';
 COMMENT ON COLUMN maevsi.upload.storage_key IS 'The upload''s storage key.';
 
-GRANT SELECT ON TABLE maevsi.upload TO maevsi_account, maevsi_tusd;
+GRANT SELECT ON TABLE maevsi.upload TO maevsi_account, maevsi_anonymous, maevsi_tusd;
 GRANT UPDATE ON TABLE maevsi.upload TO maevsi_tusd;
 GRANT DELETE ON TABLE maevsi.upload TO maevsi_tusd;
 
 ALTER TABLE maevsi.upload ENABLE ROW LEVEL SECURITY;
 
--- Display the uploads that are linked to the own account.
+-- Display
+-- - all uploads for `tusd` or
+-- - the uploads that are linked to the requesting account or
+-- - the uploads which are used as profile picture.
 CREATE POLICY upload_select_using ON maevsi.upload FOR SELECT USING (
     (SELECT current_user) = 'maevsi_tusd'
   OR
     account_id = current_setting('jwt.claims.account_id', true)::UUID
+  OR
+    id IN (SELECT upload_id FROM maevsi.profile_picture)
 );
 
 -- Only allow tusd to update rows.
@@ -37,7 +43,7 @@ CREATE POLICY upload_update_using ON maevsi.upload FOR UPDATE USING (
   (SELECT current_user) = 'maevsi_tusd'
 );
 
--- Only allow the upload's owner to delete rows.
+-- Only allow tusd to delete rows.
 CREATE POLICY upload_delete_using ON maevsi.upload FOR DELETE USING (
   (SELECT current_user) = 'maevsi_tusd'
 );
