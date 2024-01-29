cff0b7fdef045ac3f388553f7c4bbffb3a019c78
myyxl <22593897+myyxl@users.noreply.github.com>
2024-01-29 18:20:14 +0100

    feat(revert): add revert for recommendation tables

--- src/deploy/table_event_category_mapping_policy.sql
+++ src/deploy/table_event_category_mapping_policy.sql
@@ -4,11 +4,28 @@ BEGIN;
 
 GRANT SELECT, INSERT, DELETE ON TABLE maevsi.event_category_mapping TO maevsi_account;
 
--- SELECT:
---      * Every event that is public, mine or that I'm invited to
--- INSERT:
---      * My events
--- DELETE:
---      * My events
+-- Only allow selects for events authored by user, events that are public or that the user is invited to.
+CREATE POLICY event_category_mapping_select ON maevsi.event_category_mapping FOR SELECT USING (
+  NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID IS NOT NULL
+  AND (
+    event_id = (SELECT id FROM maevsi.event WHERE author_account_id = NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID)
+    OR
+    visibility = 'public'
+  )
+);
+
+-- Only allow inserts for events authored by user.
+CREATE POLICY event_category_mapping_insert ON maevsi.event_category_mapping FOR INSERT CHECK (
+  NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID IS NOT NULL
+  AND
+  event_id = (SELECT id FROM maevsi.event WHERE author_account_id = NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID)
+);
+
+-- Only allow deletes for events authored by user.
+CREATE POLICY event_category_mapping_delete ON maevsi.event_category_mapping FOR DELETE USING (
+  NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID IS NOT NULL
+  AND
+  event_id = (SELECT id FROM maevsi.event WHERE author_account_id = NULLIF(current_setting('jwt.claims.account_id', true), '')::UUID)
+);
 
 COMMIT;
--- src/revert/table_event_category_mapping_policy.sql
+++ src/revert/table_event_category_mapping_policy.sql
@@ -2,6 +2,8 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP POLICY event_category_mapping_select ON maevsi.event_category_mapping;
+DROP POLICY event_category_mapping_insert ON maevsi.event_category_mapping;
+DROP POLICY event_category_mapping_delete ON maevsi.event_category_mapping;
 
 COMMIT;
