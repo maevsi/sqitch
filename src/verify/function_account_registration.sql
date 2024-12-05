4b637cf593d6525c2e0d132d8e2dcae91216e58b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-05 08:40:25 +0100

    test(account): registration

--- src/verify/function_account_registration.sql
+++ src/verify/function_account_registration.sql
@@ -2,10 +2,24 @@
 
 BEGIN;
 
+SAVEPOINT function_privileges_for_roles;
 DO $$
 BEGIN
-  ASSERT (SELECT pg_catalog.has_function_privilege('maevsi_account', 'maevsi.account_registration(TEXT, TEXT, TEXT, TEXT)', 'EXECUTE'));
-  ASSERT (SELECT pg_catalog.has_function_privilege('maevsi_anonymous', 'maevsi.account_registration(TEXT, TEXT, TEXT, TEXT)', 'EXECUTE'));
+  IF NOT (SELECT pg_catalog.has_function_privilege('maevsi_account', 'maevsi.account_registration(TEXT, TEXT, TEXT, TEXT)', 'EXECUTE')) THEN
+    RAISE EXCEPTION 'Test function_privileges_for_roles failed: maevsi_account does not have EXECUTE privilege';
+  END IF;
+
+  IF NOT (SELECT pg_catalog.has_function_privilege('maevsi_anonymous', 'maevsi.account_registration(TEXT, TEXT, TEXT, TEXT)', 'EXECUTE')) THEN
+    RAISE EXCEPTION 'Test function_privileges_for_roles failed: maevsi_anonymous does not have EXECUTE privilege';
+  END IF;
 END $$;
+ROLLBACK TO SAVEPOINT function_privileges_for_roles;
+
+SAVEPOINT account_creation;
+DO $$
+BEGIN
+  PERFORM maevsi.account_registration('username', 'e@ma.il', 'password', 'en');
+END $$;
+ROLLBACK TO SAVEPOINT account_creation;
 
 ROLLBACK;
