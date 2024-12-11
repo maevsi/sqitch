2e294319a7b651deb1b472953ebb3ee770c5a5c6
Sven Thelemann <sven.thelemann@t-online.de>
2024-12-12 00:26:11 +0100

    feat(invitation): column names prefixed
    In the flattened invitation view, columns coming from tables contact and event now have names prefixed with contact_ and event_ resp.

--- src/deploy/view_invitation_flat.sql
+++ src/deploy/view_invitation_flat.sql
@@ -6,11 +6,16 @@
 BEGIN;
 CREATE VIEW maevsi.invitation_flat AS
 SELECT i.*,
-  c.account_id as contact_account_id, c.address, c.author_account_id as contact_author_account_id, c.email_address, c.email_address_hash,
-  c.first_name, c.last_name, c.phone_number, c.url as contact_url,
-  e.author_account_id as event_author_account_id, e.description, e.start, e.end,
-  e.invitee_count_maximum, e.is_archived, e.is_in_person, e.is_remote,
-  e.location, e.name, e.slug, e.url as event_url, e.visibility
+  c.account_id as contact_account_id, c.address as contact_address,
+  c.author_account_id as contact_author_account_id,
+  c.email_address as contact_email_address, c.email_address_hash as contact_email_address_hash,
+  c.first_name as contact_first_name , c.last_name as contact_last_name,
+  c.phone_number as contact_phone_number, c.url as contact_url,
+  e.author_account_id as event_author_account_id, e.description as event_description,
+  e.start as event_start, e.end event_end, e.invitee_count_maximum as event_invitee_count_maximum,
+  e.is_archived as event_is_archived, e.is_in_person as event_is_in_person, e.is_remote as event_is_remote,
+  e.location as event_location, e.name as event_name, e.slug as event_slug, e.url as event_url,
+  e.visibility as event_visibility
 FROM maevsi.invitation i
   JOIN maevsi.contact c ON i.contact_id = c.id
   JOIN maevsi.event e ON i.event_id = e.id
--- src/verify/view_invitation_flat.sql
+++ src/verify/view_invitation_flat.sql
@@ -4,11 +4,13 @@ BEGIN;
 
 SELECT
   id, contact_id, event_id, feedback, feedback_paper,
-  contact_account_id, address, contact_author_account_id, email_address, email_address_hash,
-  first_name, last_name, phone_number, contact_url,
-  event_author_account_id, description, "start", "end",
-  invitee_count_maximum, is_archived, is_in_person, is_remote,
-  location, name, slug, event_url, visibility
+  contact_account_id, contact_address, contact_author_account_id,
+  contact_email_address, contact_email_address_hash,
+  contact_first_name, contact_last_name, contact_phone_number, contact_url,
+  event_author_account_id, event_description,
+  event_start, event_end, event_invitee_count_maximum,
+  event_is_archived, event_is_in_person, event_is_remote,
+  event_location, event_name, event_slug, event_url, event_visibility
 FROM maevsi.invitation_flat WHERE FALSE;
 
 ROLLBACK;
