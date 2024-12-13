0186f1b95901d24cf265d6e9fabcbfb009eca32d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-13 08:11:55 +0100

    refactor(invitation): reformat flat view

--- src/deploy/view_invitation_flat.sql
+++ src/deploy/view_invitation_flat.sql
@@ -1,39 +1,40 @@
 BEGIN;
-CREATE VIEW maevsi.invitation_flat
-WITH (security_invoker)
-AS SELECT
-  i.id AS invitation_id,
-  i.contact_id AS invitation_contact_id,
-  i.event_id AS invitation_event_id,
-  i.feedback AS invitation_feedback,
-  i.feedback_paper AS invitation_feedback_paper,
-  c.id AS contact_id,
-  c.account_id AS contact_account_id,
-  c.address AS contact_address,
-  c.author_account_id AS contact_author_account_id,
-  c.email_address AS contact_email_address,
-  c.email_address_hash AS contact_email_address_hash,
-  c.first_name AS contact_first_name ,
-  c.last_name AS contact_last_name,
-  c.phone_number AS contact_phone_number,
-  c.url AS contact_url,
-  e.id AS event_id,
-  e.author_account_id AS event_author_account_id,
-  e.description AS event_description,
-  e.start AS event_start,
-  e.end AS event_end,
-  e.invitee_count_maximum AS event_invitee_count_maximum,
-  e.is_archived AS event_is_archived,
-  e.is_in_person AS event_is_in_person,
-  e.is_remote AS event_is_remote,
-  e.location AS event_location,
-  e.name AS event_name,
-  e.slug AS event_slug,
-  e.url AS event_url,
-  e.visibility AS event_visibility
-FROM maevsi.invitation i
-  JOIN maevsi.contact c ON i.contact_id = c.id
-  JOIN maevsi.event e ON i.event_id = e.id;
+CREATE VIEW maevsi.invitation_flat WITH (security_invoker) AS
+  SELECT
+    invitation.id               AS invitation_id,
+    invitation.contact_id       AS invitation_contact_id,
+    invitation.event_id         AS invitation_event_id,
+    invitation.feedback         AS invitation_feedback,
+    invitation.feedback_paper   AS invitation_feedback_paper,
+
+    contact.id                  AS contact_id,
+    contact.account_id          AS contact_account_id,
+    contact.address             AS contact_address,
+    contact.author_account_id   AS contact_author_account_id,
+    contact.email_address       AS contact_email_address,
+    contact.email_address_hash  AS contact_email_address_hash,
+    contact.first_name          AS contact_first_name ,
+    contact.last_name           AS contact_last_name,
+    contact.phone_number        AS contact_phone_number,
+    contact.url                 AS contact_url,
+
+    event.id                    AS event_id,
+    event.author_account_id     AS event_author_account_id,
+    event.description           AS event_description,
+    event.start                 AS event_start,
+    event.end                   AS event_end,
+    event.invitee_count_maximum AS event_invitee_count_maximum,
+    event.is_archived           AS event_is_archived,
+    event.is_in_person          AS event_is_in_person,
+    event.is_remote             AS event_is_remote,
+    event.location              AS event_location,
+    event.name                  AS event_name,
+    event.slug                  AS event_slug,
+    event.url                   AS event_url,
+    event.visibility            AS event_visibility
+  FROM maevsi.invitation
+    JOIN maevsi.contact ON invitation.contact_id = contact.id
+    JOIN maevsi.event   ON invitation.event_id   = event.id;
 
 COMMENT ON VIEW maevsi.invitation_flat IS 'View returning flattened invitations.';
 
