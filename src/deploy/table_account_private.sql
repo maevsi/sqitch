d36d3786a6eed54feb64f8ace35e42f925d78302
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-16 04:43:07 +0100

    feat(timestamp)!: add time zone (#92)

--- src/deploy/table_account_private.sql
+++ src/deploy/table_account_private.sql
@@ -4,14 +4,14 @@ CREATE TABLE maevsi_private.account (
   id                                         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
   birth_date                                 DATE, -- TODO: evaluate if this should be `NOT NULL` for all new accounts
-  created_at                                 TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_at                                 TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   email_address                              TEXT NOT NULL CHECK (char_length(email_address) < 255) UNIQUE, -- no regex check as "a valid email address is one that you can send emails to" (http://www.dominicsayers.com/isemail/)
   email_address_verification                 UUID DEFAULT gen_random_uuid(),
-  email_address_verification_valid_until     TIMESTAMP,
-  last_activity                              TIMESTAMP NOT NULL DEFAULT NOW(),
+  email_address_verification_valid_until     TIMESTAMP WITH TIME ZONE,
+  last_activity                              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   password_hash                              TEXT NOT NULL,
   password_reset_verification                UUID,
-  password_reset_verification_valid_until    TIMESTAMP,
+  password_reset_verification_valid_until    TIMESTAMP WITH TIME ZONE,
   upload_quota_bytes                         BIGINT NOT NULL DEFAULT 10485760 -- 10 mebibyte
 );
 
@@ -34,7 +34,7 @@ CREATE FUNCTION maevsi_private.account_email_address_verification_valid_until() 
       NEW.email_address_verification_valid_until = NULL;
     ELSE
       IF ((OLD IS NULL) OR (OLD.email_address_verification IS DISTINCT FROM NEW.email_address_verification)) THEN
-        NEW.email_address_verification_valid_until = (SELECT (NOW() + INTERVAL '1 day')::TIMESTAMP);
+        NEW.email_address_verification_valid_until = (SELECT (NOW() + INTERVAL '1 day')::TIMESTAMP WITH TIME ZONE);
       END IF;
     END IF;
 
@@ -52,7 +52,7 @@ CREATE FUNCTION maevsi_private.account_password_reset_verification_valid_until()
       NEW.password_reset_verification_valid_until = NULL;
     ELSE
       IF ((OLD IS NULL) OR (OLD.password_reset_verification IS DISTINCT FROM NEW.password_reset_verification)) THEN
-        NEW.password_reset_verification_valid_until = (SELECT (NOW() + INTERVAL '2 hours')::TIMESTAMP);
+        NEW.password_reset_verification_valid_until = (SELECT (NOW() + INTERVAL '2 hours')::TIMESTAMP WITH TIME ZONE);
       END IF;
     END IF;
 
