796727cf06352021eac7a7df02ebd01904afe956
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-18 12:05:26 +0100

    fix(role): drop before creation

--- src/deploy/role_account.sql
+++ src/deploy/role_account.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+DROP ROLE IF EXISTS maevsi_account;
 CREATE ROLE maevsi_account;
 
 GRANT maevsi_account to maevsi_postgraphile;
--- src/deploy/role_anonymous.sql
+++ src/deploy/role_anonymous.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+DROP ROLE IF EXISTS maevsi_anonymous;
 CREATE ROLE maevsi_anonymous;
 
 GRANT maevsi_anonymous to maevsi_postgraphile;
