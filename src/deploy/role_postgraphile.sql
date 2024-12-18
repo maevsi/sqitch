fecd16ea860a18f0cfdaa7f0118899acf4133cce
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-18 12:08:17 +0100

    fix(role): drop before creation (#106)

--- src/deploy/role_postgraphile.sql
+++ src/deploy/role_postgraphile.sql
@@ -1,6 +1,8 @@
 BEGIN;
 
 \set role_maevsi_postgraphile_password `cat /run/secrets/postgres_role_maevsi-postgraphile_password`
+
+DROP ROLE IF EXISTS maevsi_postgraphile;
 CREATE ROLE maevsi_postgraphile LOGIN PASSWORD :'role_maevsi_postgraphile_password';
 
 COMMIT;
