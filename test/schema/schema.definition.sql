937d255ab04313442f173804f2fd87f817249f81
sthelemann <sven.thelemann@t-online.de>
2025-01-15 15:24:13 +0100

    feat(account): add login using email address (#112)
    * feat(account): add login using email address
    
    Function`maevsi.authenticate`can now handle login in by user name as well as by email address.
    
    * feat(authenticate): reset username variable name
    
    * refactor(authenticate): use function parameter names
    
    Instead of dollar syntax.
    
    * fix(docker): correct entrypoint path
    
    * fix(plan): add dependency
    
    * test(authenticate): add savepoints
    
    * refactor(authenticate): reorder tests
    
    ---------
    
    Co-authored-by: Jonas Thelemann <e-mail@jonas-thelemann.de>

--- test/schema/schema.definition.sql
+++ test/schema/schema.definition.sql
@@ -637,29 +637,37 @@ COMMENT ON FUNCTION maevsi.achievement_unlock(code uuid, alias text) IS 'Inserts
 
 CREATE FUNCTION maevsi.authenticate(username text, password text) RETURNS maevsi.jwt
     LANGUAGE plpgsql STRICT SECURITY DEFINER
-    AS $_$
+    AS $$
 DECLARE
   _account_id UUID;
   _jwt_id UUID := gen_random_uuid();
   _jwt_exp BIGINT := EXTRACT(EPOCH FROM ((SELECT date_trunc('second', CURRENT_TIMESTAMP::TIMESTAMP)) + COALESCE(current_setting('maevsi.jwt_expiry_duration', true), '1 day')::INTERVAL));
   _jwt maevsi.jwt;
+  _username TEXT;
 BEGIN
-  IF ($1 = '' AND $2 = '') THEN
+  IF (authenticate.username = '' AND authenticate.password = '') THEN
     -- Authenticate as guest.
     _jwt := (_jwt_id, NULL, NULL, _jwt_exp, maevsi.invitation_claim_array(), 'maevsi_anonymous')::maevsi.jwt;
-  ELSIF ($1 IS NOT NULL AND $2 IS NOT NULL) THEN
-    SELECT id FROM maevsi.account WHERE account.username = $1 INTO _account_id;
+  ELSIF (authenticate.username IS NOT NULL AND authenticate.password IS NOT NULL) THEN
+    -- if authenticate.username contains @ then treat it as an email adress otherwise as a user name
+    IF (strpos(authenticate.username, '@') = 0) THEN
+      SELECT id FROM maevsi.account WHERE account.username = authenticate.username INTO _account_id;
+    ELSE
+      SELECT id FROM maevsi_private.account WHERE account.email_address = authenticate.username INTO _account_id;
+    END IF;
 
     IF (_account_id IS NULL) THEN
       RAISE 'Account not found!' USING ERRCODE = 'no_data_found';
     END IF;
+
+    SELECT account.username INTO _username FROM maevsi.account WHERE id = _account_id;
 
     IF ((
         SELECT account.email_address_verification
         FROM maevsi_private.account
         WHERE
               account.id = _account_id
-          AND account.password_hash = maevsi.crypt($2, account.password_hash)
+          AND account.password_hash = maevsi.crypt(authenticate.password, account.password_hash)
       ) IS NOT NULL) THEN
       RAISE 'Account not verified!' USING ERRCODE = 'object_not_in_prerequisite_state';
     END IF;
@@ -670,9 +678,9 @@ BEGIN
       WHERE
             account.id = _account_id
         AND account.email_address_verification IS NULL -- Has been checked before, but better safe than sorry.
-        AND account.password_hash = maevsi.crypt($2, account.password_hash)
+        AND account.password_hash = maevsi.crypt(authenticate.password, account.password_hash)
       RETURNING *
-    ) SELECT _jwt_id, updated.id, $1, _jwt_exp, NULL, 'maevsi_account'
+    ) SELECT _jwt_id, updated.id, _username, _jwt_exp, NULL, 'maevsi_account'
       FROM updated
       INTO _jwt;
 
@@ -684,7 +692,7 @@ BEGIN
   INSERT INTO maevsi_private.jwt(id, token) VALUES (_jwt_id, _jwt);
   RETURN _jwt;
 END;
-$_$;
+$$;
 
 
 ALTER FUNCTION maevsi.authenticate(username text, password text) OWNER TO postgres;
