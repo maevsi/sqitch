7245225527b7f98a76d881863c574c862254defd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-01-22 02:49:05 +0100

    fix(invoker-account-id): grant execute for tusd (#123)

--- src/deploy/function_invoker_account_id.sql
+++ src/deploy/function_invoker_account_id.sql
@@ -8,6 +8,6 @@ $$ LANGUAGE PLPGSQL STRICT SECURITY DEFINER STABLE;
 
 COMMENT ON FUNCTION maevsi.invoker_account_id() IS 'Returns the session''s account id.';
 
-GRANT EXECUTE ON FUNCTION maevsi.invoker_account_id() TO maevsi_account, maevsi_anonymous;
+GRANT EXECUTE ON FUNCTION maevsi.invoker_account_id() TO maevsi_account, maevsi_anonymous, maevsi_tusd;
 
 COMMIT;
