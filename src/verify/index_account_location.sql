7091a2e0d372ef76fba685128810bf2f7388a3cd
Sven Thelemann <sven.thelemann@t-online.de>
2025-01-22 17:26:22 +0100

    feat(location): add verifications for indexes
    Queries on dictionary tables were added to the  verification scripts for spatial indexes.

--- src/verify/index_account_location.sql
+++ src/verify/index_account_location.sql
@@ -1,3 +1,9 @@
 BEGIN;
 
+SELECT 1/COUNT(*)
+FROM   pg_class c
+JOIN   pg_namespace n ON n.oid = c.relnamespace
+WHERE  c.relname = 'idx_account_location'
+AND    n.nspname = 'maevsi_private';
+
 ROLLBACK;
