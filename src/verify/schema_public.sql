e1bed9d3eb61d32c8905a9df9c96cc944fbfa688
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-01-26 02:15:47 +0100

    fix(location): move test function to appropriate schema
    If some of there functions will be used as a feature eventually,
    they can get dragged back out of the test schema again.

--- src/verify/schema_public.sql
+++ src/verify/schema_public.sql
@@ -4,6 +4,7 @@ DO $$
 BEGIN
   ASSERT (SELECT pg_catalog.has_schema_privilege('maevsi_account', 'maevsi', 'USAGE'));
   ASSERT (SELECT pg_catalog.has_schema_privilege('maevsi_anonymous', 'maevsi', 'USAGE'));
+  ASSERT (SELECT pg_catalog.has_schema_privilege('maevsi_tusd', 'maevsi', 'USAGE'));
 END $$;
 
 ROLLBACK;
