602c096ad2973304a80638e9fe98510f0fa1a757
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-20 03:34:32 +0100

    refactor(guest): align formatting

--- src/deploy/function_guest_create_multiple.sql
+++ src/deploy/function_guest_create_multiple.sql
@@ -9,22 +9,18 @@ DECLARE
   _id UUID;
   _id_array UUID[] := ARRAY[]::UUID[];
 BEGIN
-
   FOREACH _contact_id IN ARRAY create_guests.contact_ids LOOP
-
     INSERT INTO maevsi.guest(event_id, contact_id)
-    VALUES (create_guests.event_id, _contact_id)
-    RETURNING id INTO _id;
+      VALUES (create_guests.event_id, _contact_id)
+      RETURNING id INTO _id;
 
     _id_array := array_append(_id_array, _id);
-
   END LOOP;
 
   RETURN QUERY
     SELECT *
     FROM maevsi.guest
     WHERE id = ANY (_id_array);
-
 END $$ LANGUAGE PLPGSQL STRICT SECURITY INVOKER;
 
 COMMENT ON FUNCTION maevsi.create_guests(UUID, UUID[]) IS 'Function for inserting multiple guest records.';
--- test/schema/schema.definition.sql
+++ test/schema/schema.definition.sql
@@ -836,22 +836,18 @@ DECLARE
   _id UUID;
   _id_array UUID[] := ARRAY[]::UUID[];
 BEGIN
-
   FOREACH _contact_id IN ARRAY create_guests.contact_ids LOOP
-
     INSERT INTO maevsi.guest(event_id, contact_id)
-    VALUES (create_guests.event_id, _contact_id)
-    RETURNING id INTO _id;
+      VALUES (create_guests.event_id, _contact_id)
+      RETURNING id INTO _id;
 
     _id_array := array_append(_id_array, _id);
-
   END LOOP;
 
   RETURN QUERY
     SELECT *
     FROM maevsi.guest
     WHERE id = ANY (_id_array);
-
 END $$;
 
 
