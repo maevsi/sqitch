b55aa951401f2d296008328bb0b81b5693fe7f1e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-03-19 00:00:07 +0100

    fix(account): accept legal term on registration

--- src/deploy/function_account_email_address_verification.sql
+++ src/deploy/function_account_email_address_verification.sql
@@ -35,18 +35,20 @@ CREATE FUNCTION vibetype_test.account_registration_verified (
   _email_address TEXT
 ) RETURNS UUID AS $$
 DECLARE
-  _id UUID;
+  _account_id UUID;
+  _legal_term_id UUID;
   _verification UUID;
 BEGIN
-  _id := vibetype.account_registration(_username, _email_address, 'password', 'en');
+  _legal_term_id := vibetype_test.legal_term_singleton();
+  _account_id := vibetype.account_registration(_email_address, 'en', _legal_term_id, 'password', _username);
 
   SELECT email_address_verification INTO _verification
   FROM vibetype_private.account
-  WHERE id = _id;
+  WHERE id = _account_id;
 
   PERFORM vibetype.account_email_address_verification(_verification);
 
-  RETURN _id;
+  RETURN _account_id;
 END $$ LANGUAGE plpgsql;
 
 COMMIT;
