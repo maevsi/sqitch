5bd5a9ec4bbad6d3b129bfb6d6af6881a58817a6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-04-17 05:14:51 +0200

    feat(event)!: remove existence validation function

--- src/verify/function_event_is_existing.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-DO $$
-BEGIN
-  ASSERT (SELECT pg_catalog.has_function_privilege('vibetype_account', 'vibetype.event_is_existing(UUID, TEXT)', 'EXECUTE'));
-  ASSERT (SELECT pg_catalog.has_function_privilege('vibetype_anonymous', 'vibetype.event_is_existing(UUID, TEXT)', 'EXECUTE'));
-END $$;
-
-ROLLBACK;
