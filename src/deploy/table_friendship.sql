fe052dcd6279dd774487ca8c49ab170da0191dde
Sven Thelemann <sven.thelemann@t-online.de>
2025-08-01 16:16:48 +0200

    feat(friendship)!:  redesign table `friendship`
    In the table `friendship` the columns `a_account_id` and  `b_account_id`were renamed to `account_id`and `friend_account_id, a new column `is_close_friend` was added, the policies were updated. Several friendship related functions were added. Test scripts were updated accordingly.

--- src/deploy/table_friendship.sql
+++ src/deploy/table_friendship.sql
@@ -1,10 +1,10 @@
-BEGIN;
-
 CREATE TABLE vibetype.friendship (
   id                  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-  a_account_id        UUID NOT NULL REFERENCES vibetype.account(id) ON DELETE CASCADE,
-  b_account_id        UUID NOT NULL REFERENCES vibetype.account(id) ON DELETE CASCADE,
+  account_id          UUID NOT NULL REFERENCES vibetype.account(id) ON DELETE CASCADE,
+  friend_account_id   UUID NOT NULL REFERENCES vibetype.account(id) ON DELETE CASCADE,
+
+  is_close_friend     BOOLEAN NOT NULL DEFAULT false,
   status              vibetype.friendship_status NOT NULL DEFAULT 'requested'::vibetype.friendship_status,
 
   created_at          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -12,11 +12,9 @@ CREATE TABLE vibetype.friendship (
   updated_at          TIMESTAMP WITH TIME ZONE,
   updated_by          UUID REFERENCES vibetype.account(id) ON DELETE SET NULL,
 
-  UNIQUE (a_account_id, b_account_id),
-  CONSTRAINT friendship_creator_participant CHECK (created_by = a_account_id or created_by = b_account_id),
-  CONSTRAINT friendship_creator_updater_difference CHECK (created_by <> updated_by),
-  CONSTRAINT friendship_ordering CHECK (a_account_id < b_account_id),
-  CONSTRAINT friendship_updater_participant CHECK (updated_by IS NULL or updated_by = a_account_id or updated_by = b_account_id)
+  UNIQUE (account_id, friend_account_id),
+  CONSTRAINT friendship_creator_friend CHECK (account_id <> friend_account_id),
+  CONSTRAINT friendship_creator_participant CHECK (created_by = account_id)
 );
 
 CREATE INDEX idx_friendship_created_by ON vibetype.friendship USING btree (created_by);
@@ -24,8 +22,9 @@ CREATE INDEX idx_friendship_updated_by ON vibetype.friendship USING btree (updat
 
 COMMENT ON TABLE vibetype.friendship IS 'A friend relation together with its status.';
 COMMENT ON COLUMN vibetype.friendship.id IS E'@omit create,update\nThe friend relation''s internal id.';
-COMMENT ON COLUMN vibetype.friendship.a_account_id IS E'@omit update\nThe ''left'' side of the friend relation. It must be lexically less than the ''right'' side.';
-COMMENT ON COLUMN vibetype.friendship.b_account_id IS E'@omit update\nThe ''right'' side of the friend relation. It must be lexically greater than the ''left'' side.';
+COMMENT ON COLUMN vibetype.friendship.account_id IS E'@omit update\nThe one side of the friend relation. If the status is ''requested'' then it is the requestor account.';
+COMMENT ON COLUMN vibetype.friendship.friend_account_id IS E'@omit update\nThe other side of the friend relation. If the status is ''requested'' then it is the requestee account.';
+COMMENT ON COLUMN vibetype.friendship.is_close_friend IS E'@omit create\nThe flag indicating whether account_id considers friend_account_id as a close friend or not.';
 COMMENT ON COLUMN vibetype.friendship.status IS E'@omit create\nThe status of the friend relation.';
 COMMENT ON COLUMN vibetype.friendship.created_at IS E'@omit create,update\nThe timestamp when the friend relation was created.';
 COMMENT ON COLUMN vibetype.friendship.created_by IS E'@omit update\nThe account that created the friend relation was created.';
@@ -45,20 +44,19 @@ GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE vibetype.friendship TO vibetype_ac
 
 ALTER TABLE vibetype.friendship ENABLE ROW LEVEL SECURITY;
 
--- Only allow interactions with friendships in which the current user is involved.
-CREATE POLICY friendship_existing ON vibetype.friendship FOR ALL
+CREATE POLICY friendship_not_blocked ON vibetype.friendship FOR ALL
 USING (
-  (
-    vibetype.invoker_account_id() = a_account_id
-    AND b_account_id NOT IN (SELECT id FROM vibetype_private.account_block_ids())
-  )
+  account_id NOT IN (SELECT id FROM vibetype_private.account_block_ids())
+  AND friend_account_id NOT IN (SELECT id FROM vibetype_private.account_block_ids())
+);
+
+-- Only allow interactions with friendships in which the current user is involved.
+CREATE POLICY friendship_select ON vibetype.friendship FOR SELECT
+USING (
+  account_id = vibetype.invoker_account_id()
   OR
-  (
-    vibetype.invoker_account_id() = b_account_id
-    AND a_account_id NOT IN (SELECT id FROM vibetype_private.account_block_ids())
-  )
-)
-WITH CHECK (FALSE);
+  friend_account_id = vibetype.invoker_account_id()
+);
 
 -- Only allow creation by the current user.
 CREATE POLICY friendship_insert ON vibetype.friendship FOR INSERT
@@ -66,9 +64,11 @@ WITH CHECK (
   created_by = vibetype.invoker_account_id()
 );
 
--- Only allow update by the current user and only the state transition requested -> accepted.
-CREATE POLICY friendship_update ON vibetype.friendship FOR UPDATE
+-- Only allow update by the current user if it is about accepting a friendship request.
+CREATE POLICY friendship_update_accept ON vibetype.friendship FOR UPDATE
 USING (
+  friend_account_id = vibetype.invoker_account_id()
+  AND
   status = 'requested'::vibetype.friendship_status
 ) WITH CHECK (
   status = 'accepted'::vibetype.friendship_status
@@ -76,4 +76,19 @@ USING (
   updated_by = vibetype.invoker_account_id()
 );
 
-COMMIT;
+-- Only allow update by the current user if it is already an accepted relation.
+CREATE POLICY friendship_update_toggle_closeness ON vibetype.friendship FOR UPDATE
+USING (
+  status = 'accepted'::vibetype.friendship_status
+  AND
+  account_id = vibetype.invoker_account_id()
+) WITH CHECK (
+  status = 'accepted'::vibetype.friendship_status
+  AND
+  updated_by = vibetype.invoker_account_id()
+);
+
+CREATE POLICY friendship_delete ON vibetype.friendship FOR DELETE
+USING (
+  TRUE
+);
