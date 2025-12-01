16143aa422fcd0190320929cb91b924b25cf87b6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-12-02 00:09:43 +0100

    test: events invited

--- test/logic/main.sql
+++ test/logic/main.sql
@@ -29,6 +29,7 @@ GRANT USAGE ON SCHEMA vibetype_test TO vibetype_anonymous, vibetype_account;
 
 \i scenario/database/account_block_ids.sql
 \i scenario/database/audit_log.sql
+\i scenario/database/events_invited.sql
 \i scenario/database/index_missing.sql
 \i scenario/database/index.sql
 
