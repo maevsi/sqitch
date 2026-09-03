d5260e43e1fe668dc1da12d807f5ac70f8b6a22e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-09-03 04:02:06 +0200

    fix(extension): create extensions only if absent

--- src/deploy/extension_pg_trgm.sql
+++ src/deploy/extension_pg_trgm.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE EXTENSION pg_trgm;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 COMMENT ON EXTENSION pg_trgm IS 'Provides support for similarity of text using trigram matching, also used for speeding up LIKE queries.';
 
--- src/deploy/extension_pgcrypto.sql
+++ src/deploy/extension_pgcrypto.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE EXTENSION pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 COMMENT ON EXTENSION pgcrypto IS 'Provides password hashing functions.';
 
